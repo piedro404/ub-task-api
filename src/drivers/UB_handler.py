250081968c8eda8f0754b970f73f0fdd22f8b35a
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-02-22 16:14:27 -0300

    feat: :hammer: add search task in driver UB

--- src/drivers/UB_handler.py
+++ src/drivers/UB_handler.py
@@ -1,3 +1,81 @@
 import requests
 from bs4 import BeautifulSoup
+from typing import Dict
+import re
 
+
+def email_validator(email):
+    padrao_email = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    
+    if re.match(padrao_email, email):
+        return True
+    else:
+        return False
+
+class UBHandler:
+    def __init__(self) -> None:
+        self.url_login = "https://ead.unibalsas.edu.br/login/index.php"
+
+    def __web_login(self, login: str, password: str) -> requests.Session():
+        session = requests.Session()
+
+        # POST the login form
+        response = session.get(self.url_login)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        token = soup.find('input', {'name': 'logintoken'})['value']
+
+        if(email_validator(login) == False and len(login) == 8):
+            login = f"{login[0:2]}.{login[2:3]}.{login[3:8]}"
+
+        login = {
+            'username': login,
+            'password': password,
+            'logintoken': token
+        }
+
+        session.post(self.url_login, data=login)
+
+        return session
+    
+    def ub_search_task(self, login: str, password: str) -> Dict:
+        session = self.__web_login(login, password)
+        response = session.get("https://ead.unibalsas.edu.br/calendar/view.php?view=upcoming")
+        tasks_content = BeautifulSoup(response.content, 'html.parser').find('div', class_='calendarwrapper').find_all('div', class_='details')
+
+        tasks = []
+
+        for task_content in tasks_content:
+            try:
+                url_task = task_content.find('a', class_='btn')['href']
+                status_task = BeautifulSoup(session.get(url_task).content, 'html.parser').find('div', class_='submissionstatustable').find('td', class_='cell c1 lastcol').text
+
+                if(status_task != "Nenhum envio foi feito ainda"):
+                    continue
+
+                name = task_content.find('h3').text.strip()
+                details = task_content.find('ul', class_='mb0').find_all('li')
+                day_week, date, time_limit = details[0].text.strip().split(',')
+                matter = details[2].find('a').text.strip()
+                    
+                task = {
+                    "mat": matter,
+                    "name": name,
+                    "day_week": day_week,
+                    "date": date.strip(),
+                    "time_limit": time_limit.strip(),
+                    "url_task": url_task
+                }
+                tasks.append(task)
+
+            except:
+                continue
+            
+        session.close()
+
+        return {
+            "status": True,
+            "find_task": len(tasks) > 0,
+            "description":f"Há {len(tasks)} atividade(s) pendente(s)!",
+            "amount_task": len(tasks),
+            "list_tasks": tasks
+        }
