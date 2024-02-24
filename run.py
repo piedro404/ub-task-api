d376cb4009182df57f1db61c0b17c92e60073b0e
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-02-24 11:27:32 -0300

    fix: :rewind: Leaving the run just for testing

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 from src.main.server.server import app
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=3000, debug=True)
-    app.run()
+    app.run(host='0.0.0.0', port=3000, debug=True)
