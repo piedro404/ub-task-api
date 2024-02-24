d25e6cd9f736916c05b40c64979c615d66eb83be
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-02-24 11:24:42 -0300

    fix: :rocket: Hosting project at Vercel

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from src.main.server.server import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    # app.run(host='0.0.0.0', port=3000, debug=True)
+    app.run()
