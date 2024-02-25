d9cc0ab8760a02933b048aa96ed162e1d2901107
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-02-25 00:36:44 -0300

    feat: :seedling: Add Routes Documentation, Swagger, Favicon

--- src/main/routes/app_routes.py
+++ src/main/routes/app_routes.py
@@ -24,3 +24,18 @@ def home():
             "linkedin": "pedrohenrique404"
         }
     }), 200
+
+@app_routes_bp.route('/docs')
+def swagger_ui():
+    return send_from_directory(os.path.join(app_routes_bp.root_path, '../../templates'),
+                          'docs.html')
+
+@app_routes_bp.route('/openapi.json')
+def swagger():
+    return send_from_directory(os.path.join(app_routes_bp.root_path, '../../templates/documentation'),
+                          'openapi.json')
+
+@app_routes_bp.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app_routes_bp.root_path, '../../static'),
+                          'favicon.ico')
